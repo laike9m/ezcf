9d2067fbbcb08310aed94a90de6bb4d29f646eab
laike9m <laike9m@gmail.com>
2015-03-20 21:01:40 +0800

    fix sys.path

--- tests2/run_test.py
+++ tests2/run_test.py
@@ -1,4 +1,9 @@
+import os
 import unittest
+import sys
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from subdir.subdir.test_parent import *
 
 if __name__ == '__main__':
