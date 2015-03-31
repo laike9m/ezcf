864d551ca7aaf661ecfe54cca8c69e0f9daf1c46
laike9m <laike9m@gmail.com>
2015-03-31 22:02:17 +0800

    fix license

--- ezcf/__init__.py
+++ ezcf/__init__.py
@@ -1,8 +1,7 @@
 __author__ = "laike9m (laike9m@gmail.com)"
 __title__ = 'ezcf'
 __version__ = '0.0.1'
-# __build__ = None
-__license__ = 'Apache 2.0'
+__license__ = 'MIT'
 __copyright__ = 'Copyright 2015 laike9m'
 
 import sys
