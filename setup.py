408b0ae8a4c042586d6a28625f6a7ea0d0bab115
laike9m <laike9m@gmail.com>
2015-10-17 12:56:26 +0800

    add test for Python3.5

--- setup.py
+++ setup.py
@@ -75,6 +75,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Topic :: Software Development :: Libraries :: Python Modules',
         'Programming Language :: Python :: Implementation :: PyPy',
         'Programming Language :: Python :: Implementation :: CPython',
