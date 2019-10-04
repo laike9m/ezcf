834c2fc600ffa6d705eb7646c0a52a1d8e4c49d9
laike9m <laike9m@gmail.com>
2019-10-03 22:59:37 -0700

    remove 3.6, 3.3 support

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ import codecs
 from setuptools import setup, find_packages
 from setuptools.command.test import test as TestCommand
 
+
 def read():
     """Build a file path from *paths* and return the contents."""
-    with codecs.open(os.path.join('README.rst'), 'r', "utf-8") as f:
+    with codecs.open(os.path.join("README.rst"), "r", "utf-8") as f:
         return f.read()
+
 
 class PyTest(TestCommand):
     def finalize_options(self):
@@ -17,20 +19,23 @@ class PyTest(TestCommand):
 
     def run_tests(self):
         import sys
+
         if sys.version_info[:2] == (2, 6):
-            import unittest2 as unittest    # Python 2.6
+            import unittest2 as unittest  # Python 2.6
         else:
             import unittest
 
-        setup_file = sys.modules['__main__'].__file__
+        setup_file = sys.modules["__main__"].__file__
         setup_dir = os.path.abspath(os.path.dirname(setup_file))
         tests = unittest.TestLoader().discover(
-            os.path.join(setup_dir, 'tests'), pattern='*.py')
+            os.path.join(setup_dir, "tests"), pattern="*.py"
+        )
         try:
             # https://github.com/CleanCut/green/issues/50
             from green.runner import run
             from green.suite import GreenTestSuite
             from green.config import default_args
+
             default_args.verbose = 3
             run(GreenTestSuite(tests), sys.stdout, default_args)
         except ImportError:
@@ -38,46 +43,36 @@ class PyTest(TestCommand):
 
 
 setup(
-    name='ezcf',
-    version='0.2.1',
-    description='Import JSON/YAML/INI/XML like .py files',
+    name="ezcf",
+    version="0.2.1",
+    description="Import JSON/YAML/INI/XML like .py files",
     long_description=read(),
-    url='http://github.com/laike9m/ezcf/',
-    license='MIT',
-    author='laike9m',
-    author_email='laike9m@gmail.com',
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    package_data={'': ['README.md']},
-    keywords=['config', 'import', 'JSON', 'YAML', 'INI', 'XML'],
-    install_requires=[
-        'pyyaml==5.1',
-        'configobj==5.0.6',
-        'xmltodict==0.9.2'
-    ],
-    tests_require=['coverage'],
-    extras_require={
-        'coveralls': ['coveralls']
-    },
-    cmdclass={
-        'test': PyTest,
-    },
+    url="http://github.com/laike9m/ezcf/",
+    license="MIT",
+    author="laike9m",
+    author_email="laike9m@gmail.com",
+    packages=find_packages(exclude=["contrib", "docs", "tests*"]),
+    package_data={"": ["README.md"]},
+    keywords=["config", "import", "JSON", "YAML", "INI", "XML"],
+    install_requires=["pyyaml==5.1", "configobj==5.0.6", "xmltodict==0.9.2"],
+    tests_require=["coverage"],
+    extras_require={"coveralls": ["coveralls"]},
+    cmdclass={"test": PyTest},
     include_package_data=True,
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Programming Language :: Python :: Implementation :: CPython',
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Programming Language :: Python :: Implementation :: PyPy",
+        "Programming Language :: Python :: Implementation :: CPython",
     ],
 )
